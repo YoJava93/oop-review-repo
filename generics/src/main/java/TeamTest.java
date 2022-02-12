1d65689232c7616fd4570c14ed119cd8b7fa815b
Alexandr Corenciuc <alexandrcorenciuc@gmail.com>
2022-02-11 22:11:59 -0800

    generics review

---
+++ generics/src/main/java/TeamTest.java
@@ -0,0 +1,21 @@
+public class TeamTest {
+
+    public static void main(String[] args) {
+
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        //Team liverpool = new Team("Liverpool");
+
+        Team<SoccerPlayer> liverpool = new Team<>("Liverpool");
+        liverpool.addPlayer(beckham);
+
+        //Team<String> brokenTeam = new Team<>("this wont work");
+
+        System.out.println(liverpool.numPlayers());
+
+
+    }
+
+}
